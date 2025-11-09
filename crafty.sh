ed466ade03efc0d4b0728c57a963e193061f7cb9
Björn Wahman <bjornerikzzon@gmail.com>
2025-11-09 10:05:30 +0100

    Update crafty.sh

--- crafty.sh
+++ crafty.sh
@@ -1,1 +1,1 @@
-git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git && cd crafty-installer-linux && sudo ./install_crafty.sh
+git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git && cd crafty-installer-4.0 && sudo ./install_crafty.sh
