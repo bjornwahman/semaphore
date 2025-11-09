adbb7294606767933a1d0cbb1f2973f2c767159e
Björn Wahman <bjornerikzzon@gmail.com>
2025-11-09 10:30:00 +0100

    Update crafty.sh

--- crafty.sh
+++ crafty.sh
@@ -1,3 +1,3 @@
 cd /tmp
 sudo apt -y install git python3 python3-dev python3-pip software-properties-common openjdk-8-jdk openjdk-8-jre
-git clone https://gitlab.com/crafty-controller/crafty-installer-4.0.git && cd crafty-installer-4.0 && sudo ./install_crafty.sh
+git fetch --all https://gitlab.com/crafty-controller/crafty-installer-4.0.git && cd crafty-installer-4.0 && sudo ./install_crafty.sh
